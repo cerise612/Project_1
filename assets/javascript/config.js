8335ca1c416be33da2d75b4063ed5e7e2b418eb6
cerise612 <37317705+cerise612@users.noreply.github.com>
2018-07-17 22:08:40 -0700

    Delete config.js

--- assets/javascript/config.js
+++
@@ -1,4 +0,0 @@
-var gracenoteAPIKey = "35vaqjzcmsyy66xqcyq9xvdv"
-var googleAPIKey = "AIzaSyBAw0FrpEoyC_sBDEkJNz_sPuIDvi7OxSw"
-var omdbAPIKey = "trilogy"
-var firebaseAPIKey = "AIzaSyBdwJIIy4y_fKNgZslFFjjIq1SMdc4FqNU"
