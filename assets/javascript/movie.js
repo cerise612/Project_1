1750c65754b88f189abd89cbeb7ad66a8ae897f2
Jason Forman <20210580+JRForman@users.noreply.github.com>
2018-07-19 10:53:04 -0700

    Updated movie.js

--- assets/javascript/movie.js
+++ assets/javascript/movie.js
@@ -2,19 +2,48 @@
 
 
 var votes;
+var votesTime;
+var movieTimesJSON;
+var votesInOrder;
+var movieDateAndZip;
+var omdbArray = [];
+var showVoteResults = false;
+var showTimeResults = false;
 var moviesContainer = $("#results"); //! Change to actual container
 moviesContainer.empty();
 var votingData = $("#vote"); //! Change to actual container
 
 
+database.ref("flags").on("value", function (snapshot) {
+    flags = snapshot.val();
+    if ((flags.selectedDate !== "none") && (flags.selectedZip !== "none") && (flags.movieVoting === true)) {
+        movieDate = flags.selectedDate;
+        movieZip = flags.selectedZip;
+        getDate(flags.selectedDate, flags.selectedZip)
+
+    }
+    if (flags.movieVoting) {
+        showVoteResults = true;
+
+
+    }
+    if (flags.timeVoting) {
+
+        voteMovieTimes();
+    }
+
+});
+
+
+
 
 database.ref("votes").on("value", function (snapshot) {
     votes = snapshot.val();
-    if (votes == "empty"){
+    if (votes == "empty") {
         // database.ref("votes")
     }
 
-    var votesInOrder = [];
+    votesInOrder = [];
     for (var x in votes) {
         votesInOrder.push([x, votes[x]]);
     }
@@ -22,50 +51,143 @@ database.ref("votes").on("value", function (snapshot) {
     votesInOrder.sort(function (a, b) {
         return b[1] - a[1];
     });
+<<<<<<< Updated upstream
     var win1P = $("<p>").text(votesInOrder[0][0] + ": " + votesInOrder[0][1] + " vote(s)").css("margin","auto");
     var win2P = $("<p>").text(votesInOrder[1][0] + ": " + votesInOrder[1][1] + " vote(s)").css("margin","auto");
     var win3P = $("<p>").text(votesInOrder[2][0] + ": " + votesInOrder[2][1] + " vote(s)").css("margin","auto");
+=======
+    var win1P = $("<p>").text(votesInOrder[0][0] + ": " + votesInOrder[0][1] + " votes").css("margin", "auto");
+    var win2P = $("<p>").text(votesInOrder[1][0] + ": " + votesInOrder[1][1] + " votes").css("margin", "auto");
+    var win3P = $("<p>").text(votesInOrder[2][0] + ": " + votesInOrder[2][1] + " votes").css("margin", "auto");
+>>>>>>> Stashed changes
     votingData.empty();
-    if(votesInOrder[0][1] == "0"){
-        votingData.append("<p> No voting data yet.</p>")
 
-    }else if (votesInOrder[1][1] == "0"){
-        votingData.append(win1P);
+    if (showVoteResults) {
+        if (votesInOrder[0][1] == "0") {
+            votingData.append("<p> No voting data yet.</p>")
 
-    }else if(votesInOrder[2][1] == "0"){
-        votingData.append(win1P, win2P);
 
-    }else{
-        votingData.append(win1P,win2P,win3P);
+        } else if (votesInOrder[1][1] == "0") {
+            votingData.append(win1P);
+            var winningMovie = votesInOrder[0][0];
+            database.ref().update({
+                "winningMovie": winningMovie
+            });
+
+        } else if (votesInOrder[2][1] == "0") {
+            votingData.append(win1P, win2P);
+            var winningMovie = votesInOrder[0][0];
+            database.ref().update({
+                "winningMovie": winningMovie
+            });
+
+        } else {
+            votingData.append(win1P, win2P, win3P);
+            var winningMovie = votesInOrder[0][0];
+            database.ref().update({
+                "winningMovie": winningMovie
+            });
+        }
+        var clearVotesBtn = $("<button>").text("Clear Votes").attr("id", "clearVotesBtn");
+        var endVotingBtn = $("<button>").text("End Voting").attr("id", "endMovieVotingBtn");
+        votingData.append(clearVotesBtn);
+        votingData.append(endVotingBtn);
+    }
+});
+
+database.ref("votesTime").on("value", function (snapshot) {
+    votesTime = snapshot.val();
+
+    if (votesTime == "empty") {
+        // database.ref("votes")
     }
 
-    var clearVotesBtn = $("<button>").text("Clear Votes").attr("id","clearVotesBtn");
-    var endVotingBtn = $("<button>").text("End Voting").attr("id", "endVotingBtn");
-    votingData.append(clearVotesBtn);
-    votingData.append(endVotingBtn);
+    votesInOrder = [];
+    for (var x in votesTime) {
+        votesInOrder.push([x, votesTime[x]]);
+    }
+
+    votesInOrder.sort(function (a, b) {
+        return b[1] - a[1];
+    });
+    var win1P = $("<p>").text(votesInOrder[0][0] + ": " + votesInOrder[0][1] + " votes").css("margin", "auto");
+    var win2P = $("<p>").text(votesInOrder[1][0] + ": " + votesInOrder[1][1] + " votes").css("margin", "auto");
+    var win3P = $("<p>").text(votesInOrder[2][0] + ": " + votesInOrder[2][1] + " votes").css("margin", "auto");
+    votingData.empty();
+
+    if (showTimeResults) {
+        if (votesInOrder[0][1] == "0") {
+            votingData.append("<p> No voting data yet.</p>")
+
+
+        } else if (votesInOrder[1][1] == "0") {
+            votingData.append(win1P);
+            var winningTime = votesInOrder[0][0];
+            database.ref().update({
+                "winningTime": winningTime
+            });
+
+        } else if (votesInOrder[2][1] == "0") {
+            votingData.append(win1P, win2P);
+            var winningTime = votesInOrder[0][0];
+            database.ref().update({
+                "winningTime": winningTime
+            });
+
+        } else {
+            votingData.append(win1P, win2P, win3P);
+            var winningTime = votesInOrder[0][0];
+            database.ref().update({
+                "winningTime": winningTime
+            });
+        }
+        var clearVotesBtn = $("<button>").text("Clear Votes").attr("id", "clearTimeVotesBtn");
+        var endVotingBtn = $("<button>").text("End Voting").attr("id", "endTimeVotingBtn");
+        votingData.append(clearTimeVotesBtn);
+        votingData.append(endTimeVotingBtn);
+
+
+
+}
 });
 
 
+// var movieZip = 85204; //! Get from google API
 
-var movieZip = 85204; //! Get from google API
+//! Testing
+// var movieDate = moment().format("YYYY-MM-DD"); 
+// var movieDate = "2017-07-19";
+// var movieDate = "2018-07-19";
+// var movieDate = "2018-07-20" ;
 
+// getDate(movieDate, movieZip);
+// populateMovies();
+// startMovie();
 
-var movieDate = moment().format("YYYY-MM-DD"); //! Testing
-var movieDate = "2017-07-19"; 
-var movieDate = "2018-07-19";
-var movieDate = "2018-07-20";
+function startMovie() {
+    database.ref("flags").update({
+        "movieVoting": false,
+        "timeVoting": false
+    });
+    // <p>Date: <input type="text" id="datepicker"></p>
+    moviesContainer.append('<br>Movie Date: <input class="datepicker_recurring_start" id="getDate"/>');
+    moviesContainer.append('<br>Zip-Code: <input id="getZip"/>');
+    moviesContainer.append('<button id= "submitDateAndZip">Submit</button>');
+    $(moviesContainer).on('focus', ".datepicker_recurring_start", function () {
+        $(this).datepicker();
+    });
+}
 
-// getDate(movieDate);
-
-function getDate(movieDate) {
+function getDate(movieDate, movieZip) {
+    movieDateAndZip = movieDate + '_' + movieZip;
 
     database.ref("movieTimes").once("value", function (snapshot) {
-        if (snapshot.hasChild(movieDate)) {
+        if (snapshot.hasChild(movieDateAndZip)) {
             console.log("Date in database");
 
         } else {
             var movieLookupURL = "http://data.tmsapi.com/v1.1/movies/showings?startDate=" + movieDate + "&zip=" + movieZip + "&api_key=" + gracenoteAPIKey
-            console.log("Retrieving Times for " + movieDate);
+            console.log("Retrieving Times for " + movieDate + " Zip: " + movieZip);
 
             $.ajax({
                 url: movieLookupURL,
@@ -76,12 +198,12 @@ function getDate(movieDate) {
 
                 database.ref("movieTimes").update({
 
-                    [movieDate]: movieTimes
+                    [movieDateAndZip]: movieTimes
                 });
                 for (x in response) {
                     let movieName = response[x].title;
                     if (movieName.endsWith("3D")) {
-                        console.log("3D detected");
+                        // console.log("3D detected");
                     }
 
                 }
@@ -92,18 +214,20 @@ function getDate(movieDate) {
 
 
     });
+    populateMovies();
 }
 
-populateMovies();
 
 
-function populateMovies(){
+function populateMovies() {
+    moviesContainer.empty();
     database.ref("movieTimes").once("value", function (snapshot) {
         // console.log(snapshot.val())
-    
-        var movieTimesJSON = JSON.parse(snapshot.child(movieDate).val());
+        showVoteResults = true;
+
+        movieTimesJSON = JSON.parse(snapshot.child(movieDateAndZip).val());
         // console.log(movieTimesJSON);
-    
+
         for (x in movieTimesJSON) {
             let movieNameResponse = movieTimesJSON[x].title;
             let movieID = movieTimesJSON[x].tmsId;
@@ -113,13 +237,14 @@ function populateMovies(){
                 movieName = movieNameResponse;
             }
             var movieInfoLookupURL = "https://api.themoviedb.org/3/search/movie?api_key=" + tmdbAPIKey + "&language=en-US&query=" + movieName
-    
-    
+
+
             $.ajax({
                 url: movieInfoLookupURL,
                 method: "GET"
             }).then(function (omdbResponse) {
                 // console.log(omdbResponse);
+
                 if (omdbResponse.results.length == 0) {
                     console.log("no results for:" + movieName);
                 } else {
@@ -128,40 +253,203 @@ function populateMovies(){
                     movieContainer.css("width", "14%").css("display", "inline-block");
                     var movieImg = $("<img>").attr("src", movieImgURL).addClass("poster").css("width", "90%").attr("id", movieID).attr("name", movieNameResponse).attr("vote", "false");
                     movieNameP = $("<p>").text(movieNameResponse);
+<<<<<<< Updated upstream
                     var checkMark = $("<img>").attr("src", "assets/images/checkMark.png").css("z-index", "99").css("text-align", "center").css("width", "10%").css("position", "absolute").css("transform", "translate(-105%, 10%)").attr("id", movieID + "check").css("display", "none");
     
+=======
+                    var checkMark = $("<img>").attr("src", "assets/images/checkMark.png").css("z-index", "99").css("text-align", "center").css("width", "10%").css("position", "absolute").css("transform", "translate(65%, -275%)").attr("id", movieID + "check").css("display", "none");
+                    omdbArray.push({
+                        "Name": movieNameResponse,
+                        "URL": movieImgURL
+
+                    });
+
+>>>>>>> Stashed changes
                     movieContainer.append(movieImg);
                     // movieContainer.append(movieNameP);
                     movieContainer.append(checkMark);
-    
+
                     moviesContainer.append(movieContainer);
                 }
             });
         }
+        setTimeout(function () {
+            omdbArrayStringified = JSON.stringify(omdbArray);
+
+            database.ref().update({
+                "posterURLs": omdbArrayStringified
+            });
+        }, 3000);
+
+
+
+    });
+
+}
+
+function voteMovieTimes() {
+    showVoteResults = false;
+    showTimeResults = true;
+    votingData.empty();
+
+    database.ref().once("value", function (snapshot) {
+        movieDateAndZip = (snapshot.child("flags").val()).selectedDate + "_" + (snapshot.child("flags").val()).selectedZip;
+        temp = snapshot.child("movieTimes").val()
+
+        movieTimesJSON = JSON.parse(temp[movieDateAndZip]);
+
+        posterIndexs = JSON.parse(snapshot.child("posterURLs").val());
+        winMovie = snapshot.child("winningMovie").val()
+        if (winMovie == "No votes yet 1") {
+            return;
+        }
+        var index = movieTimesJSON.findIndex(obj => obj.title == winMovie);
+
+        var movieID = movieTimesJSON[index].tmsId;
+        var showTimes = movieTimesJSON[index].showtimes;
+        var posterIndex = posterIndexs.findIndex(obj => obj.Name == winMovie);
+        var posterURL = posterIndexs[posterIndex].URL;
+        var showTimeDisplayArray = [];
+
+        var childArray = [];
+        var storeTheatre;
+        var ticketsURL;
+        for (x in showTimes) {
+            if (x == 0) {
+                storeTheatre = showTimes[x].theatre.name;
+                ticketsURL = showTimes[x].ticketURI;
+                childArray.push((showTimes[x].dateTime).slice(11, 16));
+            } else if (showTimes[x].theatre.id === showTimes[x - 1].theatre.id) {
+                childArray.push((showTimes[x].dateTime).slice(11, 16));
+            } else if (showTimes[x].theatre.id !== showTimes[x - 1].theatre.id) {
+                showTimeDisplayArray.push({
+                    "theatre": storeTheatre,
+                    "id": showTimes[x].theatre.id,
+                    "getTickets": ticketsURL,
+                    "times": childArray
+                })
+
+                storeTheatre = showTimes[x].theatre.name
+                ticketsURL = showTimes[x].ticketURI;
+                childArray = [];
+                childArray.push((showTimes[x].dateTime).slice(11, 16));
+            }
+            if (x == showTimes.length - 1) {
+                showTimeDisplayArray.push({
+                    "theatre": storeTheatre,
+                    "id": showTimes[x].theatre.id,
+                    "getTickes": ticketsURL,
+                    "times": childArray
+                })
+            }
+
+
+        }
+        showTimeDisplayArrayStringified = JSON.stringify(showTimeDisplayArray);
+        // console.log(showTimeDisplayArray);
+        database.ref().update({
+            "winingShowTimes": showTimeDisplayArrayStringified
+        });
+
+        moviesContainer.empty();
+        var movieContainer = $("<div>").css("float", "left").css("width", "40%");
+        var movieIMG = $("<img>").attr("src", posterURL).css("width", "90%");
+        moviesContainer.append("<h4>Winning Movie</h4>");
+        movieContainer.append(movieIMG);
+        movieContainer.append("<p>" + winMovie + "</p>");
+        moviesContainer.append(movieContainer);
+
+        var timesContainer = $("<div>").css("width", "40%").css("float", "left");
+        for (x in showTimeDisplayArray) {
+            timesContainer.append("<b>" + showTimeDisplayArray[x].theatre +" - ID: "+showTimeDisplayArray[x].id + "</b>")
+            for (y in showTimeDisplayArray[x].times) {
+                var str = showTimeDisplayArray[x].id +"_"+ showTimeDisplayArray[x].times[y]
+                timesContainer.append("<p class='time' vote='false' id=" + str+ ">" + showTimeDisplayArray[x].times[y] + "</p>").css("margin", "auto");
+
+            }
+
+        }
+        moviesContainer.append(timesContainer);
+
     });
 
 }
 
 
 
-
-$(votingData).on("click", function(e){
+//ON CLICKS
+$(votingData).on("click", function (e) {
     if (e.target.id == "clearVotesBtn") {
-    console.log("Votes Cleared");
-    database.ref("votes").set({
-        "No votes yet 1":"0",
-        "No votes yet 2":"0",
-        "No votes yet 3":"0"
-    });
+        console.log("Votes Cleared");
+        database.ref("votes").set({
+            "No votes yet 1": "0",
+            "No votes yet 2": "0",
+            "No votes yet 3": "0"
+        });
     }
-    if (e.target.id=="endVotingBtn"){
+    if (e.target.id == "endMovieVotingBtn") {
         console.log("Vote Ending");
+        database.ref("flags").update({
+            "movieVoting": false,
+            "timeVoting": true
+
+        });
+        voteMovieTimes();
     }
 
 })
 
 moviesContainer.on("click", function (e) {
     var targetImgID = e.target.id;
+
+    if (e.target.className == "time") {
+        var timeVote = $(e.target).attr("vote");
+        console.log(timeVote);
+        // var timeVoted = e.target.getAttribute("vote"); ✔
+        if (timeVote != "true") {
+            $(e.target).attr("vote", "true")
+            if (e.target.id in votesTime) {
+                updatedVote = (parseInt(votesTime[e.target.id]) + 1).toString();
+            } else {
+                updatedVote = "1"
+            }
+            database.ref("votesTime").update({
+                [e.target.id]: updatedVote
+            });
+            $(e.target).text("✔" + $(e.target).text());
+
+        } else {
+            $(e.target).attr("vote", "false")
+            if (e.target.id in votesTime) {
+                updatedVote = (parseInt(votesTime[e.target.id]) - 1).toString();
+            } else {
+                updatedVote = "0"
+            }
+            database.ref("votesTime").update({
+                [e.target.id]: updatedVote
+            });
+            temp = $(e.target).text()
+            temp = temp.slice(1);
+            $(e.target).text(temp)
+            
+        }
+
+    }
+
+
+    if (e.target.id == "submitDateAndZip") {
+        movieDate = moment($("#getDate").val(), "MM-DD-YYYY").format("YYYY-MM-DD");
+        movieZip = $("#getZip").val();
+
+        database.ref("flags").update({
+            "movieVoting": true,
+            "selectedDate": movieDate,
+            "selectedZip": movieZip,
+            "timeVoting": false
+
+        });
+
+    }
 
     if ((targetImgID).endsWith("check")) {
         targetImgID = targetImgID.slice(0, targetImgID.length - 5)
